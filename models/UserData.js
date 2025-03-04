7452707a923834c0963d8fb80802a6df0f89fb7b
nischay92 <nischayn92@gmail.com>
2025-03-04 11:32:30 -0500

    Set up database and fixed endpoints to store data in DB

---
+++ models/UserData.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, default: 'patient' },
+    resetToken: { type: String },
+    resetTokenExpires: { type: Date },
+}, { timestamps: true ,collection:'Users'});
+
+module.exports = mongoose.model('User', UserSchema);
